8bb6a413c1d9548d688d9b3ef55113f55a57db90
Nicolas.C.S <144803122+Nicolas1212212121@users.noreply.github.com>
2024-02-19 12:23:42 -0400

    Add files via upload

---
+++ oii/js/js.js
@@ -0,0 +1,6 @@
+function exibirnome(){
+    var gmail = document.getElementById('gmail').value;
+    var senha = document.getElementById('senha').value;
+    alert("Esse é o seu Nome:\n"+gmail+"\n\nE essa é sua Mensagem:\n"+senha)
+
+}
